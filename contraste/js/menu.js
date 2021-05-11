d57f3e687abe896be4f8dabf7ea24315f794ca2d
Vlamir Carneiro Jr <vlamir.carneiro@gmail.com>
2021-05-10 21:04:43 -0300

    Arquivos Enterprise Challenge Fase 2

---
+++ contraste/js/menu.js
@@ -0,0 +1,5 @@
+const hamburguer = document.querySelector('.hamburguer');
+   const navLinks = document.querySelector('.links__menu');  
+   hamburguer.addEventListener('click', () =>{
+    navLinks.classList.toggle('open');
+   });
